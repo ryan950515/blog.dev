11d2d263954e85265b3373e59dfc66522b919181
Y.C.Tsai <ryan950515@gmail.com>
2017-07-20 16:53:07 +0800

    laravel collective

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,23 @@
 @extends("layouts.master")
 
 @section("content")
-<form method="post" action="/posts">
+    {!! Form::open(['method'=>'post', 'action'=>'PostsController@store']) !!}
+        <div class="form-group">
+            {!! Form::label('title', '文章標題') !!}
+            {{--  <label for="title">文章標題</label>  --}}
+            {!! Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'請輸入標題']) !!}
+            {{--  <input type="text" class="form-control" name="title" id="title" placeholder="請輸入標題"/>  --}}
+        </div>
+        <div class="form-group">
+            {!! Form::label('fulltext', '文章內容'); !!}
+            {{--  <label for="fulltext">文章內容</label>  --}}
+            {!! Form::textarea('fulltext', null, ['class'=>'form-control', 'id'=>'fulltext', 'placeholder'=>'請輸入內容', 'rows'=>3]) !!}
+            {{--  <textarea class="form-control" name="fulltext" id="fulltext" placeholder="請輸入內容"/></textarea>  --}}
+        </div>
+       {!! Form::submit('存檔',['class'=>'btn btn-primary']) !!}
+        {{--  <button type="submit" class="btn btn-primary">存檔</button>  --}}
+        {!! Form::close() !!}
+{{--  <form method="post" action="/posts">
 {{csrf_field()}}
         <div class="form-group">
             <label for="title">文章標題</label>
@@ -13,7 +29,7 @@
         </div>
        
         <button type="submit" class="btn btn-primary">存檔</button>
-    </form>
+    </form>  --}}
 @endsection("content")
 
 
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,26 +1,35 @@
 @extends("layouts.master")
 
 @section("content")
-<form method="post" action="/posts/{{$post->id}}">
-{{csrf_field()}}
+    {!! Form::model($post->id,['method'=>'put', 'action'=>['PostsController@update', $post->id]]) !!}
+{{--  <form method="post" action="/posts/{{$post->id}}">  --}}
+{{--  {{csrf_field()}}  --}}
 {{-- PUT, PATCH, DELETE 要使用input hidden --}}
-    <input type="hidden" name="_method" value="PUT" />
+    {{--  <input type="hidden" name="_method" value="PUT" />  --}}
         <div class="form-group">
-            <label for="title">文章標題</label>
-            <input type="text" class="form-control" name="title" id="title" value="{{ $post->title}}"/>
+            {!! Form::label('title', '文章標題') !!}
+            {{--  <label for="title">文章標題</label>  --}}
+            {!! Form::text('title',  $post->title, ['class'=>'form-control', 'id'=>'title']) !!}
+            {{--  <input type="text" class="form-control" name="title" id="title" value="{{ $post->title}}"/>  --}}
         </div>
         <div class="form-group">
-            <label for="fulltext">文章內容</label>
-            <textarea class="form-control" name="fulltext" id="fulltext" > {{$post->fulltext}}</textarea>
+            {!! Form::label('fulltext', '文章內容'); !!}
+            {{--  <label for="fulltext">文章內容</label>  --}}
+            {!! Form::textarea('fulltext', $post->fulltext, ['class'=>'form-control', 'id'=>'fulltext',  'rows'=>3]) !!}
+            {{--  <textarea class="form-control" name="fulltext" id="fulltext" > {{$post->fulltext}}</textarea>  --}}
         </div>
-       
-        <button type="submit" class="btn btn-info">修改並存檔</button>
-    </form>
-    <form method="post" action="/posts/{{$post->id}}">
-        {{csrf_field()}}
-        <input type="hidden" name="_method" value="DELETE" />
-        <button class="btn btn-danger">刪除</button>
-    </form>
+              {!! Form::submit('修改並存檔',['class'=>'btn btn-info']) !!}
+        {{--  <button type="submit" class="btn btn-info">修改並存檔</button>  --}}
+    {{--  </form>  --}}
+    {!! Form::close() !!}
+        {!! Form::open($post->id,['method'=>'put', 'action'=>['PostsController@destory', $post->id]]) !!}
+    {{--  <form method="post" action="/posts/{{$post->id}}">  --}}
+        {{--  {{csrf_field()}}  --}}
+        {{--  <input type="hidden" name="_method" value="DELETE" />  --}}
+        {!! Form::submit('刪除',['class'=>'btn btn-danger']) !!}
+        {{--  <button class="btn btn-danger">刪除</button>  --}}
+            {!! Form::close() !!}
+    {{--  </form>  --}}
 
 @endsection("content")
 
